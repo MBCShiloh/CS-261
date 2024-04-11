58bdd13c883ad8092514a39bd2f618210bb31d67
MBCShiloh <146302034+MBCShiloh@users.noreply.github.com>
2024-04-10 19:57:35 -0700

    hello_gradescope

--- system_test.py
+++ system_test.py
@@ -1,13 +1,19 @@
 import sys
+
 original_stdout = sys.stdout
-cur_version = sys.version_info
-if cur_version >= (3, 10):
-    print("This is an acceptable version of Python, version " + str(cur_version[0]) + '.' + str(cur_version[1]) + '.')
-else:
-    print("Your Python version is too low, it needs to be 3.10 or greater and this is " + str(cur_version[0]) + '.' + str(cur_version[1]) + '.')
-
-with open('output.txt','w') as f:
+with open('output.txt', 'w') as f:
     sys.stdout = f
-    print("This will go to output.txt")
 
-    sys.stdout = original_stdout
+
+    cur_version = sys.version_info
+    if cur_version >= (3, 10):
+        print("This is an acceptable version of Python, version " + str(cur_version[0]) + '.' + str(cur_version[1]) + '.')
+    else:
+        print("Your Python version is too low, it needs to be 3.10 or greater and this is " + str(cur_version[0]) + '.' + str(cur_version[1]) + '.')
+
+    print("This will also go to output.txt")
+
+# Reset stdout
+sys.stdout = original_stdout  # Reset the standard output to its original value
+
+print("This will print to the console.")
