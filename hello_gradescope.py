dd8964374015cc412c818588305ff5173fca7fdb
MBCShiloh <146302034+MBCShiloh@users.noreply.github.com>
2024-04-10 18:36:33 -0700

    hello_gradescope

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Mark Bastion-Cavnar
+# OSU Email: bastionm@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 4/10/2024
+# Description: hello_gradescope
+
+
+def gradescope_test():
+    return 42
